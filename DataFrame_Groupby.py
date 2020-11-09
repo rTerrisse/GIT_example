775d419a8e8b2f977b20d1eb1067560d7736bb4b
Robin <robin.terrisse@le-campus-numerique.fr>
2020-11-09 16:23:45 +0100

    sort to sort_values

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -61,5 +61,5 @@ print('\n==================================================================\n')
 # Sort data ratings by created field (groupby + lambda function + sorted)
 platypus = cloneDF(mergeRatings)
 platypus = platypus.groupby(['movie_id', 'title'])['rating'].agg(
-    {'COUNT': np.size, 'myAVG': lambda x: x.sum() / float(x.count())}).sort('COUNT', ascending=False)
+    {'COUNT': np.size, 'myAVG': lambda x: x.sum() / float(x.count())}).sort_values('COUNT', ascending=False)
 print('My info sorted: \n%s' % platypus[:15])
